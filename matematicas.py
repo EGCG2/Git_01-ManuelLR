0ec6fa393d51d8166e4a1b20cca5198bd7061a60
Manuel López Ruiz <manuellr.git@gmail.com>
2016-10-21 09:49:15 +0200

    Haciendo algunos test

--- matematicas.py
+++ matematicas.py
@@ -5,4 +5,13 @@ __author__ = "Manuel López Ruiz"
 def suma(a, b):
     return a + b
 
+def resta(a, b):
+    return a - b
+
+a = 3
+b = 2
+
+print("a = " + str(a) + "; b = " + str(b))
+print("resultado suma = " + str(suma(a, b))
+print("resultado resta = " + str(resta(a, b)))
 
